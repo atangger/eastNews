9b1523c6ea3dc3274e36bf22facd036ca0e4a291
Weijie Tang <v-weijta@microsoft.com>
2018-01-23 17:35:19 +0800

    added the cluster version

--- eastmoney_cluster.js
+++ eastmoney_cluster.js
@@ -3,27 +3,23 @@ const http = require('http');
 const numCPUs = require('os').cpus().length;
 
 if(cluster.isMaster){
-	console.log('Master: Master is running');
+	console.log('Master ${process.pid} is running');
 	// Fork workers
 	var workers = new Array(numCPUs);
 	for(let i = 0; i < numCPUs; i ++){
 		workers[i] = cluster.fork();
-
-		workers[i].on('message',(m)=>{
-			console.log("Master: message received" + m['chat']);
-		});
 	}
 	setTimeout(function(){
+		console.log("get here");
 		for(let i = 0 ; i < numCPUs;i++){
-			console.log("Master: message for the " + i + " sended");
+			console.log("message for the " + i + " sended");
 			workers[i].send({id: i});
 		}
 	},3000);
 
 } else{
 	process.on('message',(m)=>{
-		console.log("worker" + process.pid + ": received msg : " + m["id"]);
-		process.send({chat: "hey master worker"+process.pid +" get the message!"});
+		console.log(process.pid + " received msg : " + m["id"]);
 	});
-	console.log("worker ${process.pid}: started ");
+	console.log("worker ${process.pid} started ");
 }
