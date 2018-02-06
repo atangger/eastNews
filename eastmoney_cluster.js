7d2892c6e46bbc2cca93564690225829989f0a33
Weijie Tang <v-weijta@microsoft.com>
2018-02-06 16:31:21 +0800

    dasdas

--- eastmoney_cluster.js
+++ eastmoney_cluster.js
@@ -152,7 +152,7 @@ if(cluster.isMaster){
 					s.push(JSON.stringify(newsUrlList));
 					s.push(null);
 
-					blob.writeStream('twjcontainertmp',m['id'] + '.json',s,(error,response) =>{
+					blob.writeStream('twjcontainer',m['id'] + '.json',s,(error,response) =>{
 						//console.log('the JSON length = ' + JSON.stringify(newsUrlList).length);
 						if(error){
 							console.log('Blob: error occur!!!');
